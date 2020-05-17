030abe7d1cd769228ae2a324f4a6ef143b05f19c
jiehan <jiehan.develop@gmail.com>
2020-05-17 16:31:27 +0800

    typo fixes

--- 04-react-redux-app/src/redux/configureStore.js
+++ 04-react-redux-app/src/redux/configureStore.js
@@ -1,6 +1,6 @@
 // Use CommonJS require below so we can dynamically import during build-time
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configreStore.prod');
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./configureStore.prod");
 } else {
-  module.exports = require('./configreStore.dev');
+  module.exports = require("./configureStore.dev");
 }
